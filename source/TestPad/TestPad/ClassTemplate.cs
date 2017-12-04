e8b0e6a2cb4229fcfeb2eef09191e7a66161f0d2
Brian J Hoskins <brian@hoskins.eu>
2017-12-04 19:43:18 +0000

    First commit

---
+++ source/TestPad/TestPad/ClassTemplate.cs
@@ -0,0 +1,32 @@
+﻿public class ClassTemplate
+{
+    #region CONSTANT FIELDS
+    #endregion
+    #region FIELDS
+    #endregion
+    #region CONSTRUCTORS
+    #endregion
+    #region DESTRUCTORS
+    #endregion
+    #region DELEGATES
+    #endregion
+    #region EVENTS
+    #endregion
+    #region ENUMS
+    #endregion
+    #region INTERFACES
+    #endregion
+    #region PROPRERTIES
+    #endregion
+    #region INDEXERS
+    #endregion
+    #region METHODS
+
+    #region EVENT HANDLERS
+    #endregion
+    #endregion
+    #region STRUCTS
+    #endregion
+    #region CLASSES
+    #endregion
+}
---
+++ source/TestPad/TestPad/TestEquipment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NationalInstruments.NI4882;
+
+namespace TestEquipment
+{
+    #region INTERFACES
+    interface IPowerSupply
+    {
+        int On();
+        int Off();
+        int SetVoltage();
+        int SetCurrent();
+        double? ReadVoltage();
+        double? ReadCurrent();
+    }
+    #endregion
+    #region TEST EQUIPMENT BASE CLASS
+    public class TestEquipment
+    {
+        #region PROPERTIES
+        /* PROPERTIES */
+        /// <summary>
+        /// Gets or sets the Test Equipment Description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description { get; protected set; }
+        #endregion
+        #region CONSTRUCTORS
+        /* CONSTRUCTORS */
+        public TestEquipment(string description = null)
+        {
+            this.Description = description;
+        }
+        #endregion
+    }
+    #endregion
+    public abstract class GPIB_Controller : TestEquipment
+    {
+        #region METHODS
+        /* METHODS */
+        public abstract void Write(string command);
+        public abstract string Read();
+        #endregion
+    }
+
+    public class NationalInstrumentsGPIB : GPIB_Controller
+    {
+        #region PROPERTIES
+        /* PROPERTIES */
+        protected NationalInstruments.NI4882.Device Instrument { get; set; }
+        #endregion
+
+    }
+}
